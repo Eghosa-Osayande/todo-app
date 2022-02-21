8bb231a7678b1cd71615f6c315bfb5f3e35ef8fe
Eghosa-Osayande <osayandeeghosam@gmail.com>
2022-02-21 20:15:06 +0100

    feat: add placeholders for TaskList empty and error states

--- lib/ui/pages/todo_list/components/todo_list_body.dart
+++ lib/ui/pages/todo_list/components/todo_list_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todo/blocs/tasks_list/task_list_bloc.dart';
 import 'package:todo/ui/pages/todo_list/components/task_item_tile.dart';
 import 'package:todo/ui/widgets/black_safe_area.dart';
+import 'package:todo/ui/widgets/retry_widget.dart';
 
 class TodoListBody extends StatelessWidget {
   const TodoListBody({
@@ -14,7 +16,14 @@ class TodoListBody extends StatelessWidget {
     return BlackSafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Todo List'),
+          centerTitle: false,
+          title: const Text(
+            'Todo List',
+            style: TextStyle(
+                fontFamily: 'circular',
+                fontSize: 18,
+                fontWeight: FontWeight.w400),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
@@ -24,22 +33,60 @@ class TodoListBody extends StatelessWidget {
           },
           child: const Icon(Icons.add),
         ),
-        body: BlocBuilder<TaskListBloc, TaskListState>(
-            builder: (context, state) {
-              
+        body:
+            BlocBuilder<TaskListBloc, TaskListState>(builder: (context, state) {
           if (state is TaskListLoadInProgress) {
-            
             return const Center(
               child: CircularProgressIndicator(),
             );
           } else if (state is TaskListLoadSuccess) {
-            return ListView.builder(
-                itemCount: state.tasks.length,
-                itemBuilder: (context, index) {
-                  return TaskItemTile(task: state.tasks[index], index: index);
-                });
+            return (state.tasks.isEmpty)
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Todo List is empty',
+                            style: TextStyle(
+                                fontFamily: 'circular',
+                                fontSize: 24,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Create a task',
+                            style: TextStyle(
+                                color: Color.fromRGBO(119, 119, 119, 1),
+                                fontFamily: 'circular',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                : ListView.builder(
+                    itemCount: state.tasks.length,
+                    itemBuilder: (context, index) {
+                      
+                      return Container(
+                        margin: EdgeInsets.only(bottom: (index==(state.tasks.length-1))?100:0),
+                        child: TaskItemTile(
+                            task: state.tasks[index], index: 1 + index),
+                      );
+                    });
           }
-          return Text('Error');
+          return RetryWidget(retryPress: (){
+            context.read<TaskListBloc>().add(
+                  TaskFetchStarted(),
+                );
+          });
         }),
       ),
     );
