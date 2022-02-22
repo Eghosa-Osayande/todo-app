30fbd1c193ad916f683f6299c421a2c58d73fd1b
Eghosa-Osayande <osayandeeghosam@gmail.com>
2022-02-22 21:46:03 +0100

    refactor: delete TodoDetailBody widget

--- lib/ui/pages/todo_detail/todo_detail_page.dart
+++ lib/ui/pages/todo_detail/todo_detail_page.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo/blocs/tasks_list/task_list_bloc.dart';
-import 'package:todo/repositories/todo/models/task_model.dart';
-import 'package:todo/ui/pages/todo_detail/components/todo_detail_body.dart';
-import 'package:todo/ui/pages/todo_list/components/todo_list_body.dart';
 import 'package:todo/ui/widgets/black_safe_area.dart';
 import 'package:todo/ui/widgets/close_keyboard.dart';
 import 'package:todo/ui/widgets/task_form.dart';
