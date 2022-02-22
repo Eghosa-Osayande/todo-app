d68e92c4fa868ff0acb3279bf66e9511ba27505e
Eghosa-Osayande <osayandeeghosam@gmail.com>
2022-02-22 20:48:11 +0100

    feat: implement buildDefaultSpinner function

---
+++ lib/ui/widgets/default_spinners.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget buildDefaultSpinner() {
+  return Center(
+    child: CircularProgressIndicator(
+      // backgroundColor: Colors.transparent,
+      color: Colors.white,
+    ),
+  );
+}
